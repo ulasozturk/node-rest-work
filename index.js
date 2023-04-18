cc9b8a4ac486645d9d70d8a2e12959a5d3abac54
Ulas Ozturk <ozulashe@gmail.com>
2023-04-18 17:29:15 +0300

    Project setup and configurations were made.

---
+++ index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ success: 1, message: "This rest api is working" });
+});
+
+app.listen(process.env.APP_PORT, () => {
+  console.log("Server up and running on PORT:", process.env.APP_PORT);
+});
