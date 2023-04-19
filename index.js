3e73cc6d9842e040f19805fdbe566eab92a124a5
Ulas Ozturk <ozulashe@gmail.com>
2023-04-19 18:38:32 +0300

    Morgan used for logging instead of Winston.

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const userRouter = require("./api/users/user.router");
-const logger = require("./config/logger");
+const rfs = require("rotating-file-stream");
+const path = require("path");
 
 app.use(express.json());
+app.use(
+  morgan("dev", {
+    skip: (req, res) => res.statusCode < 400,
+  })
+);
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "1d",
+  path: path.join(__dirname, "log"),
+});
+app.use(
+  morgan("common", {
+    stream: accessLogStream,
+  })
+);
 
 app.use("/api/users", userRouter);
 
 app.listen(process.env.APP_PORT, () => {
-  logger.info(`Server up and running on PORT: ${process.env.APP_PORT}`);
+  console.log(`Server up and running on PORT: ${process.env.APP_PORT}`);
 });
